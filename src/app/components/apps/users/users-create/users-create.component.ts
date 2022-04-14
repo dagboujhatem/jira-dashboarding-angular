b3c0b3253819fb283c01cd81b8261135f608a80a
dagbouj hatem <dagboujhatem@gmail.com>
2022-04-14 18:25:02 +0100

    add user work

--- src/app/components/apps/users/users-create/users-create.component.ts
+++ src/app/components/apps/users/users-create/users-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/shared/services/api/user.service';
 
 @Component({
   selector: 'app-users-create',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users-create.component.scss']
 })
 export class UsersCreateComponent implements OnInit {
+  public editProfile: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+     private userService: UserService,
+     private router: Router) { }
 
   ngOnInit(): void {
+    this.editProfile = this.fb.group({
+      firstName: [''],
+      lastName: [''],
+      email: ['', Validators.email],
+      role: [''],
+      password: [''],
+    })
+  }
+
+  addNewUser(){
+    this.userService.addUser(this.editProfile.value).subscribe((res:any)=>{
+      this.router.navigateByUrl('/user/index');
+    }, (err:any)=>{
+
+    });
   }
 
 }
