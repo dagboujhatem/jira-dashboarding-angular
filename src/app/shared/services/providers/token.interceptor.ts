122e2fe0e9511db0c4c1d3497ca55dcbd582b9cf
dagbouj hatem <dagboujhatem@gmail.com>
2022-04-26 23:51:47 +0100

    refactoring token interceptor with  crpting the token

--- src/app/shared/services/providers/token.interceptor.ts
+++ src/app/shared/services/providers/token.interceptor.ts
@@ -7,14 +7,16 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
+import { AuthService } from '../api/auth.service';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-  constructor(private translate: TranslateService) {}
+  constructor(private translate: TranslateService,
+    private authService: AuthService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const token = localStorage.getItem('token');
+    const token = this.authService.getToken();
     const currentLang = this.translate.currentLang;
     let headers = {};
     if(currentLang != undefined){
